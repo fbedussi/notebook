abf15673ef7304b073d52db1b8d85c957285ac91
Francesco Bedussi <francesco.bedussi@abstract.it>
2024-01-20 02:17:12 +0100

    @scope

--- list/add-note.js
+++ list/add-note.js
@@ -1,9 +1,20 @@
 import {addNote} from '../backend.js'
 import { getUserId } from '../auth.js'
-
+// import {css} from '../custom-elements-utils.js'
 customElements.define('add-note', class extends HTMLFormElement {
   constructor(){
     super()
+
+    // css`
+    //   [is="add-note"] {
+    //     button {
+    //       display: flex;
+    //       justify-content: center;
+    //       align-items: center;
+    //       gap: 0.5em;
+    //     }
+    //   }
+    // `
   }
 
   connectedCallback() {
