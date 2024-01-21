f03f817ba56cca152d51585bf9854777f8e2a52e
Francesco Bedussi <francesco.bedussi@abstract.it>
2024-01-21 18:22:34 +0100

    fix nested page reload

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,20 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  appType: 'mpa',
+  plugins: [
+    {
+      name: 'rewrite-middleware',
+      configureServer(serve) {
+        serve.middlewares.use((req, res, next) => {
+          if (req.url.startsWith('/notes/') && !req.url.includes('.js') && !req.url.includes('.css')) {
+            req.url = '/notes/'
+          }
+          next()
+        })
+      }
+    }
+  ],
   build: {
     rollupOptions: {
       input: {
