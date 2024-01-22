e76122435ef3bec66b3a1fc6fa6284390453cff7
Francesco Bedussi <francesco.bedussi@abstract.it>
2024-01-22 23:50:01 +0100

    add search params signal

--- custom-elements-utils.js
+++ custom-elements-utils.js
@@ -1,5 +1,18 @@
+import { signal } from 'https://cdn.jsdelivr.net/npm/uhtml/preactive.js'
+
 export const css = (rules) => {
   const sheet = new CSSStyleSheet();
   sheet.replaceSync(rules)
   document.adoptedStyleSheets.push(sheet)
+}
+
+export const searchParams = signal(new URLSearchParams(location.search))
+
+export const setSearchParams = (params) => {
+  const urlSearchParams = new URLSearchParams(location.search)
+  Object.entries(params).forEach(([name, value]) => {
+    urlSearchParams.set(name, value)
+  })
+  history.replaceState({}, '', `${location.pathname}?${urlSearchParams}`);
+  searchParams.value = urlSearchParams
 }
