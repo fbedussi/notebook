5d4c04708ada8ca2790c6a1ed35987ed25726835
Francesco Bedussi <francesco.bedussi@abstract.it>
2024-01-25 23:07:58 +0100

    chore: add prettier

--- notes/note-list.js
+++ notes/note-list.js
@@ -1,62 +1,74 @@
 import { render, html, signal } from 'https://cdn.jsdelivr.net/npm/uhtml/preactive.js'
-import {searchTerm} from '../state.js'
+import { searchTerm } from '../state.js'
 import { getNotes } from '../backend.js'
-import {getUserId} from '../auth.js'
-import {css} from '../custom-elements-utils.js'
+import { getUserId } from '../auth.js'
+import { css } from '../custom-elements-utils.js'
 
 import './delete-button.js'
 
-const htmlUnsafe = str => html([str]);
-customElements.define('note-list', class extends HTMLElement {
-  constructor(){
-    super()
+const htmlUnsafe = str => html([str])
+customElements.define(
+  'note-list',
+  class extends HTMLElement {
+    constructor() {
+      super()
 
-    css`
-      note-list article {
-        header,
-        footer {
-          padding: 1rem;
-        }
+      css`
+        note-list article {
+          header,
+          footer {
+            padding: 1rem;
+          }
 
-        footer {
-          display: flex;
-          justify-content: end;
-          
-          a {
+          footer {
             display: flex;
-            align-items: center;
-            gap: 0.5em;
+            justify-content: end;
+
+            a {
+              display: flex;
+              align-items: center;
+              gap: 0.5em;
+            }
           }
         }
-      }
-    `
-  }
+      `
+    }
 
-  connectedCallback() {
-    this.notes = signal([])
-    getNotes(getUserId(), this.notes)
+    connectedCallback() {
+      this.notes = signal([])
+      getNotes(getUserId(), this.notes)
 
-    render(this, this.render)
-  }
+      render(this, this.render)
+    }
 
-  render = () => html`
-      <div>
+    render = () =>
+      html` <div>
         ${this.notes.value
           // .filter(note => searchTerm.value ? note.text.includes(searchTerm.value) : true)
-          .map((note) => html`
-            <article>
-              <header style="display: flex; justify-content: end" ><delete-button id=${note.id}/></header>
-              
-              <main>
-                <h1>${note.title}</h1>
-                ${note.type === 'text' ? htmlUnsafe(note.text.substring(0,200)) : html`
-                <ol>
-                  ${note.todos.map(todo => html`<li>${todo.done} ${todo.text}</li>`)}
-                </ol>`}
-              </main>
-              
-              <footer><a is="a-route" href=${`/notes/${note.id}`}><i class="gg-details-more"></i> <i class="gg-arrow-right"></i></a></footer>
-            </article>
-          `)}
+          .map(
+            note => html`
+              <article>
+                <header style="display: flex; justify-content: end">
+                  <delete-button id=${note.id} />
+                </header>
+
+                <main>
+                  <h1>${note.title}</h1>
+                  ${note.type === 'text'
+                    ? htmlUnsafe(note.text.substring(0, 200))
+                    : html` <ol>
+                        ${note.todos.map(todo => html`<li>${todo.done} ${todo.text}</li>`)}
+                      </ol>`}
+                </main>
+
+                <footer>
+                  <a is="a-route" href=${`/notes/${note.id}`}
+                    ><i class="gg-details-more"></i> <i class="gg-arrow-right"></i
+                  ></a>
+                </footer>
+              </article>
+            `,
+          )}
       </div>`
-})
+  },
+)
