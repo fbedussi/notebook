381df8eb54fd4634dd25ed97926ca63fd7b53a3c
Francesco Bedussi <francesco.bedussi@abstract.it>
2024-02-17 10:21:59 +0100

    refactor: extract note card component

--- notes/pages/list/components/notes-list.js
+++ notes/pages/list/components/notes-list.js
@@ -1,71 +1,15 @@
 import { render, html, signal } from 'uhtml/preactive'
 import { htmlFor } from 'uhtml/keyed'
 import { searchTerm } from '../../../state.js'
-import { getNotes, updateNote } from '../../../../backend.js'
+import { getNotes } from '../../../../backend.js'
 import { getUserId } from '../../../../auth.js'
-import { css, searchParams } from '../../../../custom-elements-utils.js'
+import { searchParams } from '../../../../custom-elements-utils.js'
 
 import '../../../widgets/delete-button.js'
-
-const htmlUnsafe = str => html([str])
+import './note-card.js'
 
 const Note = (note, key) => {
-  return htmlFor(Note, key)`
-    <article>
-      <header style="display: flex; justify-content: space-between">
-        <h1>${note.title}</h1>
-        <div class="note-actions">
-          <label>
-            <input
-              type="checkbox"
-              ?checked=${note.archived}
-              onclick=${ev => {
-                updateNote({
-                  ...note,
-                  archived: ev.target.checked,
-                })
-              }}
-            />
-            Archived
-          </label>
-          <delete-button id=${note.id} />
-        </div>
-      </header>
-
-      <main>
-        ${
-          note.type === 'text'
-            ? htmlUnsafe(note.text.substring(0, 200))
-            : html`<ol>
-                ${note.todos.map(
-                  todo =>
-                    html`<li>
-                      <input
-                        type="checkbox"
-                        ?checked=${todo.done}
-                        onclick=${() => {
-                          updateNote({
-                            ...note,
-                            todos: note.todos.map(t =>
-                              t.id === todo.id ? { ...t, done: !t.done } : t,
-                            ),
-                          })
-                        }}
-                      />
-                      <span class=${todo.done ? 'todo-done' : ''}>${todo.text}</span>
-                    </li>`,
-                )}
-              </ol>`
-        }
-      </main>
-
-      <footer>
-        <a is="a-route" href=${`/notes/${note.id}`}
-          ><i class="gg-details-more"></i> <i class="gg-arrow-right"></i
-        ></a>
-      </footer>
-    </article>
-  `
+  return htmlFor(Note, key)`<note-card .note=${note} />`
 }
 
 const EL_NAME = 'notes-list'
@@ -74,32 +18,6 @@ customElements.define(
   class extends HTMLElement {
     constructor() {
       super()
-
-      css`
-        ${EL_NAME} article {
-          header,
-          footer {
-            padding: 1rem;
-          }
-
-          .note-actions {
-            display: flex;
-            gap: 1rem;
-            align-items: center;
-          }
-
-          footer {
-            display: flex;
-            justify-content: end;
-
-            a {
-              display: flex;
-              align-items: center;
-              gap: 0.5em;
-            }
-          }
-        }
-      `
     }
 
     connectedCallback() {
